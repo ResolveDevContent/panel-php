01f8d6f72ebed53c8ab94c0696ed1f712f51a381
juaniB12 <bjuan.ignacio11@gmail.com>
2024-06-05 15:07:22 -0300

    changes

---
+++ panel/read.php
@@ -0,0 +1,91 @@
+<?php
+// Check existence of id parameter before processing further
+if(isset($_GET["productId"]) && !empty(trim($_GET["productId"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM products WHERE productId = ?";
+    
+    if($stmt = mysqli_prepare($link, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_productId);
+        
+        // Set parameters
+        $param_productId = trim($_GET["productId"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                /* Fetch result row as an associative array. Since the result set
+                contains only one row, we don't need to use while loop */
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $nombre = $row["nombre"];
+                $stock = $row["stock"];
+                $precio = $row["precio"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ver Empleado</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h1>Ver Producto</h1>
+                    </div>
+                    <div class="form-group">
+                        <label>Nombre</label>
+                        <p class="form-control-static"><?php echo $row["nombre"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Stock</label>
+                        <p class="form-control-static"><?php echo $row["stock"]; ?></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Precio</label>
+                        <p class="form-control-static"><?php echo $row["precio"]; ?></p>
+                    </div>
+                    <p><a href="index.php" class="btn btn-primary">Volver</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
