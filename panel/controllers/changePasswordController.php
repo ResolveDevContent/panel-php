275b8b35b4e9eab04f1d2680967f288331b1a058
LucasMezza <mezza.lucas12@gmail.com>
2024-06-07 10:32:37 -0300

    query

--- panel/controllers/changePasswordController.php
+++ panel/controllers/changePasswordController.php
@@ -12,31 +12,42 @@ if(!empty($_POST["btnChangePassword"])) {
             $newPassword = $_POST["newPassword"];
             $rNewPassword = $_POST["rNewPassword"];
 
-            $sql = "SELECT * FROM users WHERE email='{$_SESSION['email']}' LIMIT 0,1";
-            $result = mysqli_query($link, $sql);
+            $sql = "SELECT * FROM users WHERE email = ? LIMIT 0,1";
 
-            if($result) {
-                $row = mysqli_fetch_assoc($result);
-
-                $decrypt = password_verify($currentPassword, $row['password']);
-                
-                if ($decrypt) {
-                    $hpass = password_hash($newPassword, PASSWORD_DEFAULT);
-
-                    $sql = "UPDATE users SET password='$hpass' WHERE email='{$_SESSION['email']}'";
-                    $res = mysqli_query($link, $sql);
+            if($stmt = mysqli_prepare($link, $sql)) {
+                mysqli_stmt_bind_param($stmt, "i", $_SESSION['email']);
     
-                    if($res) {
-                        $response = "La contraseña se ha actualizado con exito";
+                if(mysqli_stmt_execute($stmt)) {
+                    $result = mysqli_query($link, $sql);
+
+                    if($result) {
+                        $row = mysqli_fetch_assoc($result);
+
+                        $decrypt = password_verify($currentPassword, $row['password']);
+                        
+                        if ($decrypt) {
+                            $hpass = password_hash($newPassword, PASSWORD_DEFAULT);
+
+                            $sql = "UPDATE users SET password='$hpass' WHERE email='{$_SESSION['email']}'";
+                            $res = mysqli_query($link, $sql);
+            
+                            if($res) {
+                                $response = "La contraseña se ha actualizado con exito";
+                            } else {
+                                $response = "Ha ocurrido un error, intentelo nuevamente y si el mismo persiste comuniquese con nosotros";
+                            }
+                        } else {
+                            $response = "La contraseña actual no coincide";
+                        }
                     } else {
                         $response = "Ha ocurrido un error, intentelo nuevamente y si el mismo persiste comuniquese con nosotros";
                     }
-                } else {
-                    $response = "La contraseña actual no coincide";
+                }  else {
+                    $response = "Ha ocurrido un error, intentelo nuevamente y si el mismo persiste comuniquese con nosotros";
                 }
             } else {
                 $response = "Ha ocurrido un error, intentelo nuevamente y si el mismo persiste comuniquese con nosotros";
-            }
+            }          
         } else {
             $response = "Las nuevas contraseñas no coinciden";
         }
--- panel/controllers/loginController.php
+++ panel/controllers/loginController.php
@@ -11,24 +11,35 @@ if(!empty($_POST["btnLogin"])) {
         $email = $_POST["email"];
         $password = $_POST["password"];
 
-        $sql = "SELECT * FROM users WHERE email='$email' LIMIT 0,1";
-        $result = mysqli_query($link, $sql);
+        $sql = "SELECT * FROM users WHERE email = ? LIMIT 0,1";
 
-        if($result) {
-            if(mysqli_num_rows($result) == 0) {
-                $response = "Email y/o contraseña incorrectos";
-            } else {
-                $row = mysqli_fetch_assoc($result);
-      
-                $decrypt = password_verify($password, $row['password']);
+        if($stmt = mysqli_prepare($link, $sql)) {
+            mysqli_stmt_bind_param($stmt, "i", $email);
 
-                if ($decrypt) {
-                    $_SESSION['email'] = $row['email'];
-                    setcookie("usuarioLogeado", $email, time() + (86400 * 30), "/");
-                    header("location: index.php");
+            if(mysqli_stmt_execute($stmt)) {
+                $result = mysqli_query($link, $sql);
+
+                if($result) {
+                    if(mysqli_num_rows($result) == 0) {
+                        $response = "Email y/o contraseña incorrectos";
+                    } else {
+                        $row = mysqli_fetch_assoc($result);
+              
+                        $decrypt = password_verify($password, $row['password']);
+        
+                        if ($decrypt) {
+                            $_SESSION['email'] = $row['email'];
+                            setcookie("usuarioLogeado", $email, time() + (86400 * 30), "/");
+                            header("location: index.php");
+                        } else {
+                            $response = "Email y/o contraseña incorrectos";
+                        }
+                    }
                 } else {
-                    $response = "Email y/o contraseña incorrectos";
+                    $response = "Ha ocurrido un error, intentelo nuevamente y si el mismo persiste comuniquese con nosotros";
                 }
+            } else {
+                $response = "Ha ocurrido un error, intentelo nuevamente y si el mismo persiste comuniquese con nosotros";
             }
         } else {
             $response = "Ha ocurrido un error, intentelo nuevamente y si el mismo persiste comuniquese con nosotros";
