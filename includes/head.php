86c356c4a769539a9e53d410ca10cc7ffe166822
juaniB12 <bjuan.ignacio11@gmail.com>
2024-06-11 11:46:26 -0300

    changes

--- includes/head.php
+++ includes/head.php
@@ -2,6 +2,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="css/agendar.css">
+    <link rel="stylesheet" href="css/contacto.css">
+    <link rel="stylesheet" href="css/faq.css">
+    <link rel="stylesheet" href="css/nosotros.css">
+    <link rel="stylesheet" href="css/portafolio.css">
+    <link rel="stylesheet" href="css/unete.css">
+
+
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/icons.css">
     <link rel="stylesheet" href="css/desktop.css" media="screen and (min-width: 960px)">
