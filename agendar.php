a792cb3528fc2783d2ef6b2e6debda4bc7c988c6
juaniB12 <bjuan.ignacio11@gmail.com>
2024-06-12 11:19:40 -0300

    add agendar

--- agendar.php
+++ agendar.php
@@ -9,73 +9,80 @@
 <body>
     <?php include_once("includes/navbar.php"); ?>
     <section id="agendar" class="wrapper">
-    <header class="d-flex align-center flex-col text-center gap-5">
-        <h2>Agendá una reunión con nosotros</h2>
-        <p>
-            Para obtener más información sobre nuestros productos y servicios. No dude en enviarnos un correo electrónico. Nuestro personal siempre estará ahí para ayudarle. ¡No lo dudes!
-        </p>
-    </header>
+        <header class="d-flex align-center flex-col text-center gap-5">
+            <h2>Agendá una reunión con nosotros</h2>
+            <p>
+                Para obtener más información sobre nuestros productos y servicios. No dude en enviarnos un correo electrónico. Nuestro personal siempre estará ahí para ayudarle. ¡No lo dudes!
+            </p>
+        </header>
         <article class="d-flex align-start justify-between">
-            <form data-form>
-                <ul class="ul-form">
-                    <li>
-                        <div class="input">
-                            <input type="text" id="nombre"/>
-                            <label for="nombre">Nombre y apellido</label>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="input">
-                            <input type="text" id="telefono"/>
-                            <label for="telefono">Numero de teléfono</label>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="input">
-                            <input type="text" id="nombre-empresa"/>
-                            <label for="nombre-empresa">Nombre de la empresa</label>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="input">
-                            <input type="text" id="desc-empresa"/>
-                            <label for="desc-empresa">Descripción de la empresa (rubro y otros)</label>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="input">
-                            <select name="cotizacion" id="cotizacion">
-                                <option value="">Selecciona un motivo de la cotización</option>
-                                <option value="Servicios Personalizados">Servicios Personalizados</option>
-                                <option value="Redes Sociales">Redes Sociales</option>
-                                <option value="Diseño web">Diseño Web</option>
-                                <option value="Branding">Branding</option>
-                                <option value="Diseño grafico">Diseño Gráfico</option>
-                                <option value="Publicidad Digital">Publicidad Digital</option>
-                                <option value="Otros">Otros</option>
-                            </select>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="input">
-                            <input type="text" id="pais"/>
-                            <label for="pais">Pais o Estado</label>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="input">
-                            <input type="date" id="date">
-                        </div>
-                    </li>
-                    <li>
-                        <ul class="horarios"></ul>
-                    </li>
-                    <li>
-                        <div class="btn d-flex justify-center">
-                            <button>Enviar</button>
-                        </div>
-                    </li>
-                </ul>
+            <form data-form class="d-flex flex-col align-center gap-1 w-100">
+                <div class="d-flex align-start justify-between w-100 gap-1">
+                    <ul class="ul-form d-flex flex-col gap-1">
+                        <li>
+                            <div class="input">
+                                <input type="text" id="nombre"/>
+                                <label for="nombre">Nombre y apellido</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="input">
+                                <input type="text" id="telefono"/>
+                                <label for="telefono">Numero de teléfono</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="input">
+                                <input type="text" id="nombre-empresa"/>
+                                <label for="nombre-empresa">Nombre de la empresa</label>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="input">
+                                <input type="text" id="desc-empresa"/>
+                                <label for="desc-empresa">Descripción de la empresa (rubro y otros)</label>
+                            </div>
+                        </li>
+                        <li>
+                            <label class="label-cotizacion not-visible">Motivo de cotizaci&oacute;n</label>
+                            <div class="input">
+                                <select name="cotizacion" id="cotizacion">
+                                    <option value="">Selecciona un motivo de la cotización</option>
+                                    <option value="Servicios Personalizados">Servicios Personalizados</option>
+                                    <option value="Redes Sociales">Redes Sociales</option>
+                                    <option value="Diseño web">Diseño Web</option>
+                                    <option value="Branding">Branding</option>
+                                    <option value="Diseño grafico">Diseño Gráfico</option>
+                                    <option value="Publicidad Digital">Publicidad Digital</option>
+                                    <option value="Otros">Otros</option>
+                                </select>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="input">
+                                <input type="text" id="pais"/>
+                                <label for="pais">Pais o Estado</label>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul class="ul-form d-flex flex-col gap-1">
+                        <li>
+                            <label>Dia</label>
+                            <div class="input">
+                                <input type="date" id="date">
+                            </div>
+                        </li>
+                        <li class="item-horarios not-visible">
+                            <span>Horarios</span>
+                            <ul class="horarios d-flex align-start justify-start flex-wrap gap-5"></ul>
+                        </li>
+                    </ul>
+                </div>
+                <footer>
+                    <div class="btn d-flex justify-center">
+                        <button>Enviar</button>
+                    </div>
+                </footer>
             </form>
         </article>
     </section>
