f61824d98f7286be4cea4b70257738c930ba6fee
juaniB12 <bjuan.ignacio11@gmail.com>
2024-06-13 20:52:26 -0300

    changes

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,13 +5,43 @@
         </a>
     </div>
     <div class="d-flex align-center">
-        <ul class="d-flex align-center gap-1">
-            <li><a href="../nosotros.php">Sobre Nosotros</a></li>
-            <li><a href="../index.php#servicios">Servicios</a></li>
-            <li><a href="../portafolio.php">Portafolio</a></li>
+        <ul class="navbar-list d-flex align-center gap-1">
+            <li class="navbar-items"><a href="../nosotros.php">Sobre Nosotros</a></li>
+            <li class="navbar-items"><a href="../index.php#servicios">Servicios</a></li>
+            <li class="navbar-items"><a href="../portafolio.php">Portafolio</a></li>
         </ul>
-        <div>
+        <div class='buttons'>
             <a href="../contacto.php">Contacto</a>
         </div>
+    </div>
+
+    <div class="navbar-responsive">
+        <label for="menu">
+            <i class="icon menu"></i>
+        </label>
+        <input type="checkbox" name="menu-responsive" id="menu" />
+        <article class="popup">
+            <label class="background" for="menu"></label>
+            <ul>
+                <li class="menu-close">
+                    <em>Men&uacute;</em>
+                    <label for="menu">
+                        <i class="icon close"></i>
+                    </label>
+                </li>
+                <li>
+                    <a href="/">Inicio</a>
+                </li>
+                <li>
+                    <a href="/tienda">Tienda</a>
+                </li>
+                <li>
+                    <a href="/nosotros">Nosotros</a>
+                </li>
+                <li>
+                    <a href="/contacto">Contacto</a>
+                </li>
+            </ul>
+        </article>
     </div>
 </nav>
