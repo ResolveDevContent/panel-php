565b584fe2a4321b8e529d1093dfa61cf288c7cb
LucasMezza <mezza.lucas12@gmail.com>
2024-06-18 12:52:48 -0300

    proyectos

--- portafolio.php
+++ portafolio.php
@@ -4,6 +4,7 @@
     <?php
         $title = "Protafolio";
         include_once "includes/head.php";  
+        require_once "config.php";
         
         $destacados = '<div class="d-flex flex-col align-center justify-center empty-state">
                         <span>No hay proyectos destacados por el momento</span>
@@ -13,70 +14,69 @@
                         <span>No hay proyectos disponibles por el momento</span>
                      </div>';
 
-        // $query = "SELECT * FROM proyectos";
-        // if($result = mysqli_query($sql, $query)){
-        //     if(mysqli_num_rows($result) > 0){
-                // $pDestacados = array_filter($result, function($var) { return ($var == true); });
-                // $pNoDestacados = array_filter($result, function($var) { return ($var == false); });
+        $query = "SELECT * FROM proyectos WHERE destacado = 0";
+        if($result = mysqli_query($sql, $query)) {
+            if(mysqli_num_rows($result) > 0) {
+                $proyectos = '<ul class="gap-1 w-100">';
+                while($row = mysqli_fetch_array($result)) {
+                    $proyectos .= '<li>';
+                        $proyectos .= "<a href='/proyecto.php?proyectoId=". $row['proyectoId'] ."'>";
+                            $proyectos .= '<span class="loader"></span>';
+                            $proyectos .= "<img src='". $row['portada'] ."' alt=''>";
+                            $proyectos .= "<p>'". $row['servicio']. "'</p>";
+                        $proyectos .= '</a>';
+                    $proyectos .= '</li>';
+                }
+                $proyectos .= '</ul>';
+            }
+        }
 
-                // if($pNoDestacados && count($pNoDestacados) > 0) {
-                    //         $proyectos = '<ul class="gap-1">';
-                                //  foreach($pNoDestacados as $row) {
-                    //                 $proyectos .= '<li>';
-                    //                     $proyectos .= "<a href='/proyecto.php?proyectoId=". $row['proyectoId'] ."'>";
-                    //                         $proyectos .= '<span class="loader"></span>';
-                    //                         $proyectos .= "<img src='". $row['portada'] ."' alt=''>";
-                    //                         $proyectos .= "<p>'". $row['servicio']. "'</p>";
-                    //                     $proyectos .= '</a>';
-                    //                 $proyectos .= '</li>';
-                    //             }
-                    //         $proyectos .= '</ul>';
-                // }
-
-                // if($pDestacados && count($pDestacados) > 0) {
-                //     $destacados = '<ul>';
-                //     foreach($pNoDestacados as $row) {
-                //         $destacados .= '<li>';
-                //             $destacados .= '<article class="d-flex proyecto">';
-                //                 $destacados .= "<a href='/proyecto.php?proyectoId=". $row['proyectoId'] ."'>"
-                //                     $destacados .= "<img src='". $row['portada'] ."' alt=''>";
-                //                 $destacados .= '</a>';
-                //                 $destacados .= '<div class="d-flex flex-col align-start">';
-                //                     $destacados .= '<div class="d-flex flex-col align-start">';
-                //                         $destacados .= "<em>'". $row['nombre'] ."'</em>";
-                //                         $destacados .= "<span>'". $row['servicio'] ."'</span>";
-                //                     $destacados .= '</div>';
-                //                     $destacados .= '<ul class="d-flex align-center gap-5">';
-                //                         $destacados .= '<li>';
-                //                             $destacados .= '<div class="btn">';
-                //                                 $destacados .= "<a href='". $row['facebook']."' target='_blank'>";
-                //                                     $destacados .= '<i class="icon facebook"></i>';
-                //                                 $destacados .= '</a>';
-                //                             $destacados .= '</div>';
-                //                         $destacados .= '</li>';
-                //                         $destacados .= '<li>';
-                //                             $destacados .= '<div class="btn">';
-                //                                 $destacados .= "<a href='". $row['instagram']."' target='_blank'>";
-                //                                     $destacados .= '<i class="icon instagram"></i>';
-                //                                 $destacados .= '</a>';
-                //                             $destacados .= '</div>';
-                //                         $destacados .= '</li>';
-                //                         $destacados .= '<li>';
-                //                             $destacados .= '<div class="btn">';
-                //                                 $destacados .= "<a href='". $row['website']."' target='_blank'>";
-                //                                     $destacados .= '<i class="icon world"></i>';
-                //                                 $destacados .= '</a>';
-                //                             $destacados .= '</div>';
-                //                         $destacados .= '</li>';
-                //                     $destacados .= '</ul>';
-                //                 $destacados .= '</div>';
-                //             $destacados .= '</article>';
-                //         $destacados .= '</li>';
-                //     }
-                //     $destacados .= '</ul>';
-                // }
-        //     }
-        // }
+        $aQuery = "SELECT * FROM proyectos WHERE destacado = 1";
+        if($res = mysqli_query($sql, $aQuery)) {
+            if(mysqli_num_rows($res) > 0) { 
+                $destacados = '<ul class="w-100">';
+                while($aRow = mysqli_fetch_array($res)) {
+                    $destacados .= '<li>';
+                        $destacados .= '<article class="d-flex proyecto">';
+                            $destacados .= "<a href='/proyecto.php?proyectoId=". $aRow['proyectoId'] ."'>";
+                                $destacados .= "<span class='loader'></span>";
+                                $destacados .= "<img src='". $aRow['portada'] ."' alt=''>";
+                            $destacados .= '</a>';
+                            $destacados .= '<div class="d-flex flex-col align-start">';
+                                $destacados .= '<div class="d-flex flex-col align-start">';
+                                    $destacados .= "<em>'". $aRow['nombre'] ."'</em>";
+                                    $destacados .= "<span>'". $aRow['servicio'] ."'</span>";
+                                $destacados .= '</div>';
+                                $destacados .= '<ul class="d-flex align-center gap-5">';
+                                    $destacados .= '<li>';
+                                        $destacados .= '<div class="btn">';
+                                            $destacados .= "<a href='". $aRow['facebook'] ."' target='_blank'>";
+                                                $destacados .= '<i class="icon facebook"></i>';
+                                            $destacados .= '</a>';
+                                        $destacados .= '</div>';
+                                    $destacados .= '</li>';
+                                    $destacados .= '<li>';
+                                        $destacados .= '<div class="btn">';
+                                            $destacados .= "<a href='". $aRow['instagram'] ."' target='_blank'>";
+                                                $destacados .= '<i class="icon instagram"></i>';
+                                            $destacados .= '</a>';
+                                        $destacados .= '</div>';
+                                    $destacados .= '</li>';
+                                    $destacados .= '<li>';
+                                        $destacados .= '<div class="btn">';
+                                            $destacados .= "<a href='". $aRow['website'] ."' target='_blank'>";
+                                                $destacados .= '<i class="icon world"></i>';
+                                            $destacados .= '</a>';
+                                        $destacados .= '</div>';
+                                    $destacados .= '</li>';
+                                $destacados .= '</ul>';
+                            $destacados .= '</div>';
+                        $destacados .= '</article>';
+                    $destacados .= '</li>';
+                }
+                $destacados .= '</ul>';
+            }
+        }
     ?>
 
     <body>
--- proyecto.php
+++ proyecto.php
@@ -3,58 +3,87 @@
 
     <?php
         $title = "Protafolio";
-        include_once "includes/head.php";  
+        include_once "includes/head.php";
+        require_once "config.php";
         
-        // $proyectoImagenes = '';
-        // $servicio = '<div class="d-flex flex-col align-center justify-center empty-state">
-        //                 <span>La informaci&oacute;n no se encuntra disponible</span>
-        //             </div>';
+        $proyectoImagenes = '';
+        $redes = '<em class="vertical-text">Resultados del proyecto</em>';
+        $servicio = '<div class="d-flex flex-col align-center justify-center empty-state">
+                        <span>La informaci&oacute;n no se encuntra disponible</span>
+                    </div>';
 
-        // if(isset($_GET["proyectoId"]) && !empty(trim($_GET["proyectoId"]))){
-            // $query = "SELECT * FROM proyectos WHERE proyectoId = ?";
+        if(isset($_GET["proyectoId"]) && !empty(trim($_GET["proyectoId"]))){
+            $query = "SELECT * FROM proyectos WHERE proyectoId = ?";
     
-            // if($stmt = mysqli_prepare($sql, $query)) {
-            //     mysqli_stmt_bind_param($stmt, "i", $param);
+            if($stmt = mysqli_prepare($sql, $query)) {
+                mysqli_stmt_bind_param($stmt, "i", $param);
     
-            //     $param = $_GET["proyectoId"];
+                $param = $_GET["proyectoId"];
     
-            //     if(mysqli_stmt_execute($stmt)) {
-            //         $result = mysqli_stmt_get_result($stmt);
+                if(mysqli_stmt_execute($stmt)) {
+                    $result = mysqli_stmt_get_result($stmt);
     
-            //         if($result) {
-            //             if(mysqli_num_rows($result) > 0) {
-                            // $row = mysqli_fetch_assoc($result);
+                    if($result) {
+                        if(mysqli_num_rows($result) > 0) {
+                            $row = mysqli_fetch_assoc($result);
 
-                            // $proyecto = '<header class="d-flex flex-col text-center">
-                            //                 <h2>'. $row['nombre'] .'</h2>
-                            //                 <span>'. $row['servicio'] .'</span>
-                            //             </header>
-                            //             <div class="d-flex flex-col">
-                            //                 <em>'. $row['descripcion'] .'</em>
-                            //                 <span></span>
-                            //                 <div class="d-flex align-center gap-5">
-                            //                     <i class="icon mail"></i>
-                            //                     <span>'. $row['mail'] .'</span>
-                            //                 </div>
-                            //             </div>';
+                            $proyecto = '<header class="d-flex flex-col text-center">
+                                            <h2>'. $row['nombre'] .'</h2>
+                                            <span>'. $row['servicio'] .'</span>
+                                        </header>
+                                        <div class="d-flex flex-col align-center text-center">
+                                            <em>'. $row['descripcion'] .'</em>
+                                            <span></span>
+                                            <div class="d-flex align-center gap-5">
+                                                <i class="icon mail"></i>
+                                                <span>'. $row['email'] .'</span>
+                                            </div>
+                                        </div>';
 
-                            // $proyectoImagenes = "<ul class='d-flex align-center' data-scrollable>
-            //                         <li>
-            //                             <span class='loader'></span>
-            //                             <img src='{$row['portada']}' alt=''>
-            //                         </li>';
-            //             } else {
-                            // header("location: 404page.php");
-                        // }
-            //         } else {
-                        // header("location: 404page.php");
-                    // }
-            //     } else {
-                    // header("location: 404page.php");
-                // }
-            // } else {
-                // header("location: 404page.php");
-            // }
+                            if($row['facebook'] || $row['instagram'] || $row['website']) {
+                                $redes = '<em class="vertical-text">Redes Sociales</em>';
+                                $redes .= '<ul class="d-flex aling-center flex-col gap-5">';
+                                if($row['facebook']) {
+                                    $redes .= '<li>';
+                                        $redes .= "<a href='". $row['facebook'] ."' class='d-flex align-center' target='_blank' title='Facebook'>";
+                                            $redes .= '<i class="icon facebook"></i>';
+                                        $redes .= '</a>';
+                                    $redes .= '</li>';
+                                }
+                                if($row['instagram']) {
+                                    $redes .= '<li>';
+                                        $redes .= "<a href='". $row['instagram'] ."' class='d-flex align-center' target='_blank' title='Instagram'>";
+                                            $redes .= '<i class="icon instagram"></i>';
+                                        $redes .= '</a>';
+                                    $redes .= '</li>';
+                                }
+                                if($row['website']) {
+                                    $redes .= '<li>';
+                                        $redes .= "<a href='". $row['website'] ."' class='d-flex align-center' target='_blank' title='Website'>";
+                                            $redes .= '<i class="icon world"></i>';
+                                        $redes .= '</a>';
+                                    $redes .= '</li>';
+                                }
+                                $redes .= '</ul>';
+                            }
+
+                            $proyectoImagenes = '<ul class="d-flex align-center" data-scrollable>';
+                                $proyectoImagenes .= '<li>';
+                                    $proyectoImagenes .= '<span class="loader"></span>';
+                                    $proyectoImagenes .= "<img src='". $row['portada'] ."' alt=''>";
+                                $proyectoImagenes .= '</li>';
+                        } else {
+                            header("location: 404page.php");
+                        }
+                    } else {
+                        header("location: 404page.php");
+                    }
+                } else {
+                    header("location: 404page.php");
+                }
+            } else {
+                header("location: 404page.php");
+            }
 
             // $query = "SELECT * FROM imagenesProyuectos WHERE proyectoId = ?";
 
@@ -68,22 +97,22 @@
     
             //         if($result) {
             //             if(mysqli_num_rows($result) > 0) {
-        //                     while($row = mysqli_fetch_array($result)){
-        //                         $proyectoImagenes .= '<li>';
-        //                             $proyectoImagenes .= '<span class="loader"></span>';
-        //                             $proyectoImagenes .= "<img src='". $row['portada'] ."' alt=''>";
-        //                         $proyectoImagenes .= '</li>';
-        //                     }
+            //                 while($row = mysqli_fetch_array($result)){
+            //                     $proyectoImagenes .= '<li>';
+            //                         $proyectoImagenes .= '<span class="loader"></span>';
+            //                         $proyectoImagenes .= "<img src='". $row['portada'] ."' alt=''>";
+            //                     $proyectoImagenes .= '</li>';
+            //                 }
             //             }
             //         }
             //     }
             // }
 
-            // $proyectoImagenes .= '</ul>';
+            $proyectoImagenes .= '</ul>';
 
-        // } else {
-            // header("location: 404page.php");
-        // }
+        } else {
+            header("location: 404page.php");
+        }
     ?>
 
     <body>
@@ -92,32 +121,12 @@
         <section id="banner">
             <div>
                 <aside class="d-flex align-center flex-col justify-around">
-                    <em class="vertical-text">Redes Sociales</em>
-                    <ul class="d-flex aling-center flex-col gap-5">
-                        <li>
-                            <a href="#" class="d-flex align-center" target="_blank" title="Facebook">
-                                <i class="icon facebook"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="d-flex align-center" target="_blank" title="Instagram">
-                                <i class="icon instagram"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="d-flex align-center" target="_blank" title="Sitio Web">
-                                <i class="icon website"></i>
-                            </a>
-                        </li>
-                    </ul>
+                    <?php 
+                        echo $redes;
+                    ?>
                 </aside>
                 <nav class="nav-arrows d-flex align-center justify-between" data-arrows>
-                    <a href="#" data-arrow="-1" class="d-flex">
-                        <i class="icon arrow-left"></i>
-                    </a>
-                    <a href="#" data-arrow="1" class="d-flex">
-                        <i class="icon arrow-right"></i>
-                    </a>
+                    
                 </nav>
                 <?php
                     echo $proyectoImagenes;
