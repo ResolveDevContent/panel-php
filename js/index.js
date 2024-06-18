78472d414b637de601272cc8367745a6bf43641b
juaniB12 <bjuan.ignacio11@gmail.com>
2024-06-18 10:03:59 -0300

    changes

--- js/index.js
+++ js/index.js
@@ -43,12 +43,12 @@ document
             .querySelectorAll('[data-paises]')
             .forEach(function(row) {
 
-                let selectPaises = `<select name="pais" id="paises" requiered>
+                let selectPaises = `<select name="paises" id="paises" requiered>
                             <option value="">Selecciona un pais</option>`;
 
                 if(paises && paises.length > 0) {
                     paises.forEach(function(pais) {
-                        selectPaises += `<option value="+${pais.phone_code}">${pais.nombre}</option>`;
+                        selectPaises += `<option value="+${pais.phone_code}" data-pais="${pais.nombre}">${pais.nombre}</option>`;
                     })
                 }
                 
@@ -67,8 +67,19 @@ document
                         .forEach(function(input) {
                             input.value = row.value;
                     });
+                    
+                    root
+                    .querySelectorAll('#pais')
+                    .forEach(function(input) {
+                        const option = row.querySelector(`option[value="${row.value}"]`);
+
+                        if(option) {
+                            input.value = option.dataset.pais;
+                        }
+                    })
                 });
             });
+
 });
 
 // FAQS ----------------------------------------------------------------------
--- utils/mailer.php
+++ utils/mailer.php
@@ -12,12 +12,12 @@
         $msg = "De: $nombre<br>";
         $msg .= "Asunto: Cotización<br><br>";
         $msg .= "Datos:";
-        $msg .= "<p>Nombre y apellido:" . $nombre . "</p><br>";
-        $msg .= "<p>Teléfono:" . $telefono . "</p><br>";
-        $msg .= "<p>Empresa:" . $empresa . "</p><br>";
-        $msg .= "<p>Descripcion de empresa:" . $descripcion . "</p><br>";
-        $msg .= "<p>Pais:" . $pais . "</p><br>";
-        $msg .= "<p>Cotización:" . $cotizacion . "</p><br>";
+        $msg .= "<p>Nombre y apellido: " . $nombre . "</p><br>";
+        $msg .= "<p>Teléfono: " . $telefono . "</p><br>";
+        $msg .= "<p>Empresa: " . $empresa . "</p><br>";
+        $msg .= "<p>Descripcion de empresa: " . $descripcion . "</p><br>";
+        $msg .= "<p>Pais: " . $pais . "</p><br>";
+        $msg .= "<p>Cotización: " . $cotizacion . "</p><br>";
         $msg .= "--<p>Este mensaje fue enviado a trav&eacute;s de un formulario de contacto.</p>--";
     } else if($contacto) {
         $msg = "De: $nombre <a href='mailto:$email'>$email</a><br>";
@@ -29,24 +29,24 @@
         $msg = "De: $nombre <a href='mailto:$email'>$email</a><br>";
         $msg .= "Asunto: Agendar reunion<br><br>";
         $msg .= "Datos:";
-        $msg .= "<p>Nombre y apellido:" . $nombre . "</p><br>";
-        $msg .= "<p>Teléfono:" . $telefono . "</p><br>";
-        $msg .= "<p>Empresa:" . $empresa . "</p><br>";
-        $msg .= "<p>Descripcion de empresa:" . $descripcion . "</p><br>";
-        $msg .= "<p>Pais:" . $pais . "</p><br>";
-        $msg .= "<p>Cotización:" . $cotizacion . "</p><br>";
-        $msg .= "<p>Dia:" . $dia . "</p><br>";
-        $msg .= "<p>Horario:" . $horario . "</p><br>";
+        $msg .= "<p>Nombre y apellido: " . $nombre . "</p><br>";
+        $msg .= "<p>Teléfono: " . $telefono . "</p><br>";
+        $msg .= "<p>Empresa: " . $empresa . "</p><br>";
+        $msg .= "<p>Descripcion de empresa: " . $descripcion . "</p><br>";
+        $msg .= "<p>Pais: " . $pais . "</p><br>";
+        $msg .= "<p>Cotización: " . $cotizacion . "</p><br>";
+        $msg .= "<p>Dia: " . $dia . "</p><br>";
+        $msg .= "<p>Horario: " . $horario . "</p><br>";
         $msg .= "--<p>Este mensaje fue enviado a trav&eacute;s de un formulario de contacto.</p>--";
     } else if($unete){
         $msg = "De: $nombre <a href='mailto:$email'>$email</a><br>";
         $msg .= "Asunto: Trabajá con nosotros<br><br>";
         $msg .= "Datos:";
-        $msg .= "<p>Nombre y apellido:" . $nombre . "</p><br>";
-        $msg .= "<p>Correo electrónico:" . $email . "</p><br>";
-        $msg .= "<p>Teléfono:" . $telefono . "</p><br>";
-        $msg .= "<p>Pais:" . $pais . "</p><br>";
-        $msg .= "<p>Puesto:" . $puesto . "</p><br>";
+        $msg .= "<p>Nombre y apellido: " . $nombre . "</p><br>";
+        $msg .= "<p>Correo electrónico: " . $email . "</p><br>";
+        $msg .= "<p>Teléfono: " . $telefono . "</p><br>";
+        $msg .= "<p>Pais: " . $pais . "</p><br>";
+        $msg .= "<p>Puesto: " . $puesto . "</p><br>";
         $msg .= "--<p>Este mensaje fue enviado a trav&eacute;s de un formulario de contacto.</p>--";
     }
 
