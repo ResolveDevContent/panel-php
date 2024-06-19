b1806859c743af1ff35cbc1b8bb1155af03e1237
LucasMezza <mezza.lucas12@gmail.com>
2024-06-19 14:58:11 -0300

    mobile css

--- includes/footer.php
+++ includes/footer.php
@@ -127,7 +127,10 @@
             </ul>
             <ul class="contacto d-flex align-start justify-center gap-1 flex-col">
                 <li class="d-flex align-center gap-5">
-                    <i class="icon mail" title="Mail"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 4.7-8 5.334L4 8.7V6.297l8 5.333 8-5.333V8.7z"></path>
+                </svg>
+                    <!-- <i class="icon mail" title="Mail"></i> -->
                     <span>contacto@digitalredlimit.com</span>
                 </li>
                 <li>
