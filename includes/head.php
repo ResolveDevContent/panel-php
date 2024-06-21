654d3f02e0c3b6b273ca32928cdf2f524bfb2edd
juaniB12 <bjuan.ignacio11@gmail.com>
2024-06-21 16:51:20 -0300

    add favicon

--- includes/head.php
+++ includes/head.php
@@ -22,5 +22,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <link rel="icon" type="image/x-icon" href="/panel-php/images/favicon.ico">
     <title><?php echo $title; ?> - RedLimit</title>
 </head>
